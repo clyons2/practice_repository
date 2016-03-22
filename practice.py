b6382f882eaea0f6030c301ce0fab2ca78a602fc
clyons2 <clyons@nebrwesleyan.edu>
2016-03-22 10:56:17 -0500

    Created function to run FizzBuzz loop earlier

--- practice.py
+++ practice.py
@@ -5,3 +5,16 @@ for n in range(25):
 		print('Fizz')
 	if n % 5 == 0:
 		print('Buzz')
+		
+print('Edit')
+
+def FizzBuzz(x):
+	for n in range(x):
+		if n % 3 == 0:
+			if n % 5 == 0:
+				print('FizzBuzz')
+			print('Fizz')
+		if n % 5 == 0:
+			print('Buzz')
+			
+print(FizzBuzz(200))
